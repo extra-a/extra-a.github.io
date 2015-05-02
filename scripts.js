9c696338fb626235776ee0a78f4ff16bd9062df1
aaa <aaa@aaa.local>
2015-05-02 23:49:54 +0300

    Back to top.

--- scripts.js
+++ scripts.js
@@ -12,6 +12,21 @@ jQuery(document).ready(function ($) {
                     }
                   };
     var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+    var offset = 200;
+    var duration = 250;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+    jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+    jQuery('.back-to-top').hide();
     $.get("https://raw.githubusercontent.com/extra-a/sauer-sdl2-bins/master/VERSION").then(function(data) {
         $("#version").html($(data.results[0], "p").text());
     });
